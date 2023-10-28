48fc52ae9f14551b7fdb92d6610aec8a3d120bf4
Juan Leyes <juanleyes0610@gmail.com>
2023-10-27 21:47:49 -0300

    render de noticias

--- assets/data.js
+++ assets/data.js
@@ -1,90 +1,111 @@
-const newsData = [
-    {
-        id: 1,
-        title: "Así será la final del Mundial de Rugby 2023",
-        date: "21/10/2023",
-        synopsis: "Tras el triunfo de los All Blacks y Sudáfrica, se enfrentarán este 28 en Saint-Denis.",
-        category: "deporte",
-        cardImg: "./assets/img/news/IMG-allBlacksVsSudafrica.jpg",
-    },
-    {
-        id: 2,
-        title: "Verstappen, sin frenos en la temporada.",
-        date: "22/10/2023",
-        synopsis: "Max Verstappen junto a Red Bull consiguió hoy su decimoquinta victoria de la temporada, y la número 50 de su carrera en la F1.",
-        category: "deporte",
-        cardImg: "./assets/img/news/IMG-maxVerstappen.jpg",
-    },
-    {
-        id: 3,
-        title: "Luciana Moyano debutó con victoria en los Juegos Panamericanos",
-        date: "23/10/2023",
-        synopsis: "La cordobesa de 18 años logró avanzar a la segunda instancia luego de enfrentarse a Ana Zamburek.",
-        category: "deporte",
-        cardImg: "./assets/img/news/IMG-lucianaMoyano.jpg",
-    },
-    {
-        id: 4,
-        title: "Kanye West y su ruptura con Adidas",
-        date: "08/08/2023",
-        synopsis: "Luego de conductas erráticas y dichos antisemitas por parte del artista, la marca anuncia que no venderá mas prodcutos Yeezy despues de 2023.",
-        category: "entretenimiento",
-        cardImg: "./assets/img/news/IMG-kanyeWest.jpg",
-    },
-    {
-        id: ,
-        title: ,
-        date: ,
-        synopsis: ,
-        category: ,
-        cardImg: ,
-    },
-    {
-        id: ,
-        title: ,
-        date: ,
-        synopsis: ,
-        category: ,
-        cardImg: ,
-    },
-    {
-        id: ,
-        title: ,
-        date: ,
-        synopsis: ,
-        category: ,
-        cardImg: ,
-    },
-    {
-        id: ,
-        title: ,
-        date: ,
-        synopsis: ,
-        category: ,
-        cardImg: ,
-    },
-    {
-        id: ,
-        title: ,
-        date: ,
-        synopsis: ,
-        category: ,
-        cardImg: ,
-    },
-    {
-        id: ,
-        title: ,
-        date: ,
-        synopsis: ,
-        category: ,
-        cardImg: ,
-    },
-    {
-        id: ,
-        title: ,
-        date: ,
-        synopsis: ,
-        category: ,
-        cardImg: ,
-    },
-]
+const reportsData = [
+  {
+    id: 1,
+    title: "Luciana Moyano debutó con victoria en los Juegos Panamericanos.",
+    date: "23/10/2023",
+    synopsis:
+      "La cordobesa de 18 años logró avanzar a la segunda instancia luego de enfrentarse a Ana Zamburek.",
+    category: "deporte",
+    cardImg: "./assets/img/news/IMG-lucianaMoyano.jpg",
+  },
+  {
+    id: 2,
+    title: "Verstappen, sin frenos en la temporada.",
+    date: "22/10/2023",
+    synopsis:
+      "Max Verstappen junto a Red Bull consiguió hoy su decimoquinta victoria de la temporada, y la número 50 de su carrera en la F1.",
+    category: "deporte",
+    cardImg: "./assets/img/news/IMG-maxVerstappen.jpg",
+  },
+  {
+    id: 3,
+    title: "Así será la final del Mundial de Rugby 2023.",
+    date: "21/10/2023",
+    synopsis:
+      "Tras el triunfo de los All Blacks y Sudáfrica, se enfrentarán este 28 en Saint-Denis.",
+    category: "deporte",
+    cardImg: "./assets/img/news/IMG-allBlacksVsSudafrica.jpg",
+  },
+  {
+    id: 4,
+    title: "Twitch permitirá la transmisión simultanes con otras plataformas.",
+    date: "21/10/2023",
+    synopsis:
+      "La plataforma permitirá a sus creadores realizar transmisiones simultania en cualquier otro servicio de streaming, lo que es un gran cambio en sus políticas.",
+    category: "entretenimiento",
+    cardImg: "./assets/img/news/IMG-twitch.jpg",
+  },
+  {
+    id: 5,
+    title: "La polemica tarifa de 1 dolar anual para usuarios de X.",
+    date: "18/10/2023",
+    synopsis:
+      "El nuevo plan pretende que quienes paguen la suscripcion puedan publicar mensajes y quienes no, solo podran leerlos. El fin es reducir la cantidad de bots en la plataforma.",
+    category: "tecnologia",
+    cardImg: "./assets/img/news/IMG-X.jpg",
+  },
+  {
+    id: 6,
+    title: "Ver el vaso medio lleno es la actitud para ahorrar agua.",
+    date: "16/10/2023",
+    synopsis:
+      "El uso de ingedientes marinos, brigadas de limpieza marina y proteccion se semillas y restaurantes con mínimo de desechos, el Día Mundial de la Alimentación propone maneras de manejar el agua.",
+    category: "politica",
+    cardImg: "./assets/img/news/IMG-cuidarAgua.jpg",
+  },
+  {
+    id: 7,
+    title: "Samsung presenta el Galaxy S23 Fe.",
+    date: "04/10/2023",
+    synopsis:
+      "El lanzamiento se destacó por mantener las características y calidad del tope de gama pero a un precio más económico, pues la linea 'Fan Edition' busca ofrecer caracteristicas premium a un precio accesible.",
+    category: "tecnologia",
+    cardImg: "./assets/img/news/IMG-S23FE.jpg",
+  },
+  {
+    id: 8,
+    title: "Las 'microcasas' que alivian la situacion de los sin techo.",
+    date: "28/08/2023",
+    synopsis:
+      "Se tratan de una estrategia para refugiar y prindar alimentos a la poblacion sin techo de Sao Paulo, Brasil.",
+    category: "politica",
+    cardImg: "./assets/img/news/IMG-microcasas.jpg",
+  },
+  {
+    id: 9,
+    title: "Kanye West y su ruptura con Adidas.",
+    date: "08/08/2023",
+    synopsis:
+      "Luego de conductas erráticas y dichos antisemitas por parte del artista, la marca anuncia que no venderá mas prodcutos Yeezy despues de 2023.",
+    category: "entretenimiento",
+    cardImg: "./assets/img/news/IMG-kanyeWest.jpg",
+  },
+  {
+    id: 10,
+    title: "'DATA' de Tainy sube al No. 2 de Top Latin Albums.",
+    date: "11/07/2023",
+    synopsis:
+      "El productor puertorriqueño vuelve al top 10 de Top Latin Albums de Billboards luego de 16 años con su último álbum Data.",
+    category: "entretenimiento",
+    cardImg: "./assets/img/news/IMG-dataTainy.jpg",
+  },
+  {
+    id: 11,
+    title:
+      "Las mujeres estan representadas en todos los parlamentos del mundo.",
+    date: "06/03/2023",
+    synopsis:
+      "Por primera vez en la historia, hay mujeres en todos los parlamentos nacionales del mundo, según datos de la Union Interparlamentaria (UIP) en su último reporte anual.",
+    category: "politica",
+    cardImg: "./assets/img/news/IMG-mujeresParlamentos.jpg",
+  },
+  {
+    id: 12,
+    title: "Google Bard ya disponible en español.",
+    date: "13/07/2023",
+    synopsis:
+      "La nueva IA de Google ya se encuentra disponible en más de 40 idiomas. La tecnologia permite presonalizar la experiencia, estimular la creatividad y realizar una variendad de actividades.",
+    category: "tecnologia",
+    cardImg: "./assets/img/news/IMG-googleBard.jpg",
+  },
+];
--- assets/index.js
+++ assets/index.js
@@ -2,16 +2,51 @@
 const menuBtn = document.querySelector(".menu__label");
 // Menu (Hamburguesa)
 const barsMenu = document.querySelector(".navbar__list");
+// Contenedor de noticias
+const reportsContainer = document.querySelector(".news__container");
 
-console.log(menuBtn, barsMenu);
-
+//Fc menu hamburguesa
 const toggleMenu = () => {
   barsMenu.classList.toggle("open-menu");
+};
+
+// Fc html template
+const createReportTemplate = (report) => {
+  const { id, title, date, synopsis, cardImg } = report;
+  return `<div class="new">
+  <img
+    src="${cardImg}"
+    alt="${title}"
+  />
+  
+  <div class="new__info">
+    <div class="new-top">
+      <p>${date}</p>
+      <h3>${title}</h3>
+    </div>
+    <div class="new-mid">
+      <p>${synopsis}</p>
+    </div>
+    <div class="new-bot">
+      <button class="more__btn">Leer más</button>
+      <i
+        class="fa-regular fa-bookmark fav__btn"
+        style="color: #000000"
+      ></i>
+    </div>
+  </div>
+  </div>`;
+};
+
+// Fc para renderizar noticias
+const renderReports = (reportsList) => {
+  reportsContainer.innerHTML += reportsList.map(createReportTemplate).join("");
 };
 
 //Funcion init
 const init = () => {
   menuBtn.addEventListener("click", toggleMenu);
+  renderReports(reportsData);
 };
 
 init();
